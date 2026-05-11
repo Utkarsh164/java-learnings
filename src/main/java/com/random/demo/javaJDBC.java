c007e61cb794b4aff806ea63a14457c6ad5f4cfc
Utkarsh Singh Rawat <utkarsh.srawat2003@gmail.com>
2026-05-11 17:50:44 +0530

    multi data print

--- src/main/java/com/random/demo/javaJDBC.java
+++ src/main/java/com/random/demo/javaJDBC.java
@@ -8,16 +8,21 @@ public class javaJDBC {
 		String url="jdbc:postgresql://localhost:5432/demo";
 		String username="postgres";
 		String password="P@ssword@123";
-		String sql="select * from student where sid=1";
+		String sql="select * from student";
 		Connection con = DriverManager.getConnection(url,username,password);
 		System.out.println("Connection Established");
 		
 		Statement st=con.createStatement();
 		ResultSet rs=st.executeQuery(sql);
+		//next must hei warna error milega
+		//System.out.println("rs : "+rs.next());
 		
-		System.out.println("rs : "+rs.next());
-		
-		System.out.println(rs.getString("name"));
+//		System.out.println(rs.getString("name"));
+		while(rs.next()) {
+			System.out.print(rs.getInt("sid")+" ");
+			System.out.print(rs.getString("name")+" ");
+			System.out.println(rs.getInt("mark"));
+		}
 		
 		System.out.println("Connection closed");
 		con.close();
